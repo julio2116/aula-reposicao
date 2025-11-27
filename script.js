6dfdfd7fc685264a693f00cbe210ede0979401ad
julio2116 <juliobatista2116@gmail.com>
2025-11-26 21:58:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const label = document.querySelector('#email-label')
+const input = document.querySelector('#email')
+
+input.addEventListener('focus', () => {
+    label.style = 'top: -10px'
+    console.log(input.value)
+})
+
+input.addEventListener('blur', () => {
+    if(input.value == ""){
+        label.style = 'top: 1px'
+    }
+})
+
